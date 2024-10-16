7a960fb8b3b70cd01f848b2a8a38b54026b7dc85
Swapnamoy Bhowmick <swapnamoy31@gmail.com>
2024-10-15 22:11:29 -0400

    ran flake8

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
